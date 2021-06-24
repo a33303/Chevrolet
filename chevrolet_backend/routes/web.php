b26bbd03132f13c677c03d1d96d872f174f157e6
a3330 <a33303@mail.ru>
2021-06-24 23:29:17 +0300

    Chevrolet_fronted

--- chevrolet_backend/routes/web.php
+++ chevrolet_backend/routes/web.php
@@ -17,3 +17,13 @@ Artisan::call('storage:link'); //add local storage link
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Setting up Vue in Laravel
+//run [composer require laravel/ui]
+//Install Vue - [php artisan ui vue]
+//if you Install Vue with auth use [php artisan ui vue --auth]
+//run [npm install].
+//run [npm run dev].
+//run PHP artisan serve.
+
